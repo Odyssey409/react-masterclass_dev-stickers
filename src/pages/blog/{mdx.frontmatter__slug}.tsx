84f840ce2aef8cab5d22d52c11d53696662aee33
Odyssey409 <77610388+Odyssey409@users.noreply.github.com>
2024-09-10 20:28:31 +0900

    section#10.13 Blog Post
    자동 생성 된 각 페이지의 내용을 prop으로 받아와서 MDX를 통해 제목 내용등을 HTML형식으로 자동 변환 받고 페이지에 그려주는 기능 구현

--- src/pages/blog/{mdx.frontmatter__slug}.tsx
+++ src/pages/blog/{mdx.frontmatter__slug}.tsx
@@ -1,10 +1,35 @@
+import { graphql } from "gatsby";
 import Layout from "../../components/Layout";
 import React from "react";
+import Seo from "../../components/Seo";
 
-export default function BlogPost() {
+interface IBlogPostProps {
+  data: Queries.PostDetailsQuery;
+  children: any;
+}
+export default function BlogPost({ data, children }: IBlogPostProps) {
   return (
-    <Layout title="Blog Post">
-      <div></div>
+    <Layout title="">
+      <div>{children}</div>
     </Layout>
   );
 }
+
+export const query = graphql`
+  query PostDetails($frontmatter__slug: String) {
+    mdx(frontmatter: { slug: { eq: $frontmatter__slug } }) {
+      body
+      frontmatter {
+        author
+        category
+        date
+        title
+        slug
+      }
+    }
+  }
+`;
+
+export const Head = ({ data }: IBlogPostProps) => (
+  <Seo title={data.mdx?.frontmatter?.title} />
+);
