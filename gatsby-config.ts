6a07260c0054a1286be097b5866f4d3199f662e8
Odyssey409 <77610388+Odyssey409@users.noreply.github.com>
2024-09-11 03:01:25 +0900

    netlify.toml 파일 생성

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -15,7 +15,7 @@ const config: GatsbyConfig = {
     {
       resolve: `gatsby-source-contentful`,
       options: {
-        spaceId: `hu0lbfbw3xw2`,
+        spaceId: process.env.CONTENTFUL_SPACE_ID,
         // Learn about environment variables: https://gatsby.dev/env-vars
         accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
       },
