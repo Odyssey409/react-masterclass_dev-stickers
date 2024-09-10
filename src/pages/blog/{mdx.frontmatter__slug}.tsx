a773874ce26585c11b13ab38ecad1daa6e37d452
Odyssey409 <77610388+Odyssey409@users.noreply.github.com>
2024-09-10 21:05:47 +0900

    section#10.15 GatsbyImage
    샘플 이미지 다운로드
    https://unsplash.com/photos/SymZoeE8quA
    https://unsplash.com/photos/gJJhG4gM7NA
    
    Non-null assertion operator
    data.mdx?.frontmatter?.headerImage?.childImageSharp?.gatsbyImageData! 에서 뒤에 !는 Non-null assertion operator로 피연산자가 Nullish(null이나 undefined) 값이 아니라고 타입스크립트에게 단언하는 것입니다. 즉, 앞의 값이 null 또는 undefined가 아님을 확신할 때 사용할 수 있습니다.
    (하지만 ! 대신 if문을 사용하는 것을 추천)
    
    GatsbyImage
    ```
    headerImage {
    childImageSharp {
    gatsbyImageData(height: 300, placeholder: BLURRED)
    }
    }
    ```

--- src/pages/blog/{mdx.frontmatter__slug}.tsx
+++ src/pages/blog/{mdx.frontmatter__slug}.tsx
@@ -2,14 +2,19 @@ import { graphql } from "gatsby";
 import Layout from "../../components/Layout";
 import React from "react";
 import Seo from "../../components/Seo";
+import { GatsbyImage, getImage } from "gatsby-plugin-image";
 
 interface IBlogPostProps {
   data: Queries.PostDetailsQuery;
   children: any;
 }
 export default function BlogPost({ data, children }: IBlogPostProps) {
+  const image = getImage(
+    data.mdx?.frontmatter?.headerImage?.childImageSharp?.gatsbyImageData!
+  );
   return (
     <Layout title="">
+      <GatsbyImage image={image as any} alt="Header Image" />
       <div>{children}</div>
     </Layout>
   );
@@ -25,6 +30,11 @@ export const query = graphql`
         date
         title
         slug
+        headerImage {
+          childImageSharp {
+            gatsbyImageData(height: 600, placeholder: BLURRED)
+          }
+        }
       }
     }
   }
