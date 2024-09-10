a8c3d7fcd6703b79ad4c4eb859a02b3c0bbdcf13
Odyssey409 <77610388+Odyssey409@users.noreply.github.com>
2024-09-11 02:25:53 +0900

    section#10.17 Product Detail
    graphQL으로 부터 상품 상세 데이터를 받아온 후, 그걸 활용하여 상품 상세 페이지를 구성 함

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ import * as React from "react";
 import Seo from "../components/Seo";
 import Layout from "../components/Layout";
 import { GatsbyImage, getImage, StaticImage } from "gatsby-plugin-image";
-import { graphql, Page, PageProps } from "gatsby";
+import { graphql, Link, Page, PageProps } from "gatsby";
 
 export default function IndexPage({ data }: PageProps<Queries.StickersQuery>) {
   return (
@@ -13,8 +13,10 @@ export default function IndexPage({ data }: PageProps<Queries.StickersQuery>) {
             image={getImage(sticker.preview?.gatsbyImageData!)!}
             alt={sticker.name!}
           />
-          <h2>{sticker.name}</h2>
-          <h4>${sticker.price}</h4>
+          <Link to={`/products/${sticker.id}`}>
+            <h2>{sticker.name}</h2>
+            <h4>${sticker.price}</h4>
+          </Link>
         </article>
       ))}
     </Layout>
@@ -24,6 +26,7 @@ export const query = graphql`
   query Stickers {
     allContentfulStickerPack {
       nodes {
+        id
         name
         price
         preview {
